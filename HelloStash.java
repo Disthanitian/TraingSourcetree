c8c28020df961b16928a05c37220f26582560ee8
Thiti.Prove <proove.theory@hotmail.com>
2021-02-20 18:11:13 +0700

    [STORY-02]: add more print line

--- HelloStash.java
+++ HelloStash.java
@@ -1,9 +1,15 @@
 public class MyClass {
     public static void main(String args[]) {
-      int x=10;
+      System.out.println("Sum of x+y = " + z);
+	  int x=10;
       int y=25;
       int z=x+y;
 
       System.out.println("Sum of x+y = " + z);
+	  System.out.println("Sum of x+y = " + z);
+	  System.out.println("Sum of x+y = " + z);
+	  System.out.println("Sum of x+y = " + z);
+	  System.out.println("Sum of x+y = " + z);
+	  System.out.println("Sum of x+y = " + z);
     }
 }
